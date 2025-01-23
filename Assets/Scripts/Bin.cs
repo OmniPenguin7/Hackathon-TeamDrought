313d5952bcdce9875f24fa94d7d2297f6696e9a6
Charlie KERSHAW <2438220@Swansea.ac.uk>
2025-01-23 22:14:25 +0000

    Final

--- Assets/Scripts/Bin.cs
+++ Assets/Scripts/Bin.cs
@@ -5,7 +5,16 @@ using UnityEngine;
 
 public class Bin : MonoBehaviour
 {
+    public GameObject binEffect;
+
+    public Transform binEffectTransform;
+
     public List<GameObject> rubbish = new List<GameObject>(); // List to store collected items
+
+    [SerializeField]
+    private string targetTag = "Player"; // Tag of the object that should trigger the audio
+
+    public AudioSource audioSource; // Reference to the AudioSource component
     void OnTriggerEnter(Collider other)
     {
         // Check if the colliding object is something you want to collect
@@ -13,6 +22,46 @@ public class Bin : MonoBehaviour
         {
             rubbish.Add(other.gameObject);
             Destroy(other.gameObject); // Destroy the object after collection
+
+            Instantiate(binEffect, binEffectTransform.position, transform.rotation);
+            Debug.Log("bin effect working");
+
+            audioSource.Play();
+
+
+            
+            
+
+
         }
     }
+
 }
+
+//public class PlayAudioOnObjectEnter : MonoBehaviour
+//{
+//    [SerializeField]
+//    private string targetTag = "Player"; // Tag of the object that should trigger the audio
+
+//    public AudioSource audioSource; // Reference to the AudioSource component
+
+//    private void Start()
+//    {
+//        // Get the AudioSource component
+//        audioSource = GetComponent<AudioSource>();
+//        if (audioSource == null)
+//        {
+//            Debug.LogError("No AudioSource found on this GameObject. Please add one.");
+//        }
+//    }
+
+//    private void OnTriggerEnter(Collider other)
+//    {
+//        // Check if the object entering the trigger has the correct tag
+//        if (other.CompareTag(targetTag) && audioSource != null)
+//        {
+//            // Play the audio
+//            audioSource.Play();
+//        }
+//    }
+//}
--- Assets/Scripts/Teleport with grab.cs
+++ Assets/Scripts/Teleport with grab.cs
@@ -43,4 +43,7 @@ public class TeleportOnGrab : MonoBehaviour
         }
 
     }
+
+
+
 }
